244c9c024256f391e109bd99c0482ea334fd2082
Quack-co <>
2026-02-26 16:05:53 +0000

    Missing content

---
+++ function_return_value_specification/missing_content/main.py
@@ -0,0 +1,12 @@
+# Working with Generator functions
+def bus_announcements(stops):
+    while True:
+        for stop in stops:
+            yield f"Next stop: {stop}" 
+
+stops_list = ["Central Station", "Main Street", "Park Avenue", "Airport"]
+announcer = bus_announcements(stops_list)
+
+# Testing the result
+for _ in range(6):
+    print(next(announcer))
